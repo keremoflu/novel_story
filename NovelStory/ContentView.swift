3d9dda03fbbd72ded686523f4d6e21f61f42d6f0
Kerem Oflu <keremoflu92@gmail.com>
2025-06-27 22:29:02 +0300

    Create Project

---
+++ NovelStory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NovelStory
+//
+//  Created by Kerem on 27.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, Novel App!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NovelStory/NovelStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovelStoryApp.swift
+//  NovelStory
+//
+//  Created by Kerem on 27.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct NovelStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
